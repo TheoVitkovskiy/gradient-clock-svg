4f411702cfbe97d30989b7208d1372e32d22f77c
fedorvitkovskiy <fedor.vitkovskiy@thetimemaster.de>
2020-07-03 16:27:15 +0200

    - add muted, hidden, autoplay video to keep app alive

--- js/index.js
+++ js/index.js
@@ -54,6 +54,7 @@ const setTimerOnClick = () => {
 }
 
 const clockOnClick = (e) => {
+    window.navigator.vibrate(10);
     const fruit = getClickedFruit(e.currentTarget);
     updateFruit(fruit);
 }
@@ -170,7 +171,7 @@ const playFaviconAlarm = () => {
 }
 
 const playVibrateAlarm = () => {
-    window.navigator.vibrate([30, 100]);
+    window.navigator.vibrate([40, 30, 100]);
 }
 
 const toggleFavicon = () => {
