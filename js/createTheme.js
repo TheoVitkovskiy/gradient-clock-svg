864ca978b3e8e56ecdc006412475f5ab7eb0ca3e
TheoVitkovskiy <fvitkovski@mail.de>
2020-07-21 12:51:11 +0200

    first dark/light theme switch without click

--- js/createTheme.js
+++ js/createTheme.js
@@ -1,15 +1,45 @@
 import themeConfig from '../theme.js'
+import {setDocProperty} from './helpers.js'
 const buildTheme = () => {
-    const darkTheme = themeConfig.dark;
-    const lightTheme = themeConfig.light;
-    const variableNames = getVariableNames(darkTheme);
+    const THEME = "dark"
+    const theme = themeConfig[THEME];
 
+    setThemeCSS(theme)
+    // setThemeCSS(lightTheme)
+    // const variableNames = getVariableNames(darkTheme);
+    // console.log(getAllKeyValues(darkTheme));
 
 }
+const setThemeCSS = (obj) => {
+    
+    const keys = Object.keys(obj);
+    keys.forEach((key) => {
+        const value = obj[key];
+        setDocProperty("--" + key, value)
+    }) 
+}
+// document.querySelector("i-toggle-themes").addEventListener("click", () => {
+   
+//     setThemeCSS(keys)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*first attemt to flatten object*/
 const getVariableNames = (theme) => {
     const keys = Object.keys(theme);
     const allKeys = getAllKeys(theme);
-
 }
 const getAllKeys = (obj) => {
     const allKeys = Object.keys(obj);
@@ -24,6 +54,36 @@ const getAllKeys = (obj) => {
     
     return allKeys;
 }
+const getAllKeyValues = (obj) => {
+    const allKeyValues = {};
+    const allKeys = Object.keys(obj);
+
+    allKeys.forEach((key) => {
+        let value = obj[key];
+
+        let fullKey = '';
+        while(isObject(value)) {
+            fullKey += key;
+            value = value[key];
+            console.log(fullKey);
+            console.log(value);
+        }
+
+        allKeyValues[fullKey] = value;
+    });
+
+    return allKeyValues;
+}
+
+const getFullKey = (key, ) => {
+
+}
+
+const isObject = (obj) => {
+    return obj === Object(obj);
+}
+
+
 
 
 export default buildTheme;
--- theme.js
+++ theme.js
@@ -1,13 +1,28 @@
 export default
 {
     "dark": {
-        "rectangle": {
-            "box-shadow-color": "rgba(255, 255, 255, 0.4)"
-        }
+       "body-color": "#29364c",
+      "container-after-bg": "no-repeat center/100% url(/img/bg-static-blurM.svg), #f1f3f534",
+      "container-bg": "no-repeat 50% 27%/47% url(img/apple-big6.svg),no-repeat 20% 30%/57% url(img/bg-moving-svg-final.svg),  var(--bg-color)",
+      "testDiv-z-index": "12",
+      "testDiv-bg": "linear-gradient(90deg, var(--body-color) 5%, rgba(255,255,255,0) 50%, var(--body-color) 95%)",
+      "bg-moving-opacity": "0.3",
+      "bg-moving-z-index": "11",
+      "clock-container-box-color":"rgba(65, 105, 225)",
+      "rectangle-box-color": "#29364c",  
+      "rectangle-box-shadow-color": "rgba(255, 255, 255, 0.4)"
     },
+
     "light": {
-        "rectangle": {
-            "box-shadow-color": "rgba(0, 0, 0, 0.4)"
-        }
+      "body-color": "transparent",
+      "container-after-bg": "no-repeat center/80% url(/img/bg-static-blur4.svg)",
+      "container-bg": "",
+      "testDiv-z-index": "0",
+      "testDiv-bg": "linear-gradient(90deg, rgba(255,255,255,1) 5%, rgba(255,255,255,0) 50%, rgba(255,255,255,1) 95%)",
+      "bg-moving-opacity": "0.5",
+      "bg-moving-z-index": "-11",
+      "clock-container-box-color": "rgb(152, 172, 231)",
+      "rectangle-box-color": "rgba(65, 105, 225)",
+      "rectangle-box-shadow-color": "rgba(0, 0, 0, 0.4)"
     }
 }
